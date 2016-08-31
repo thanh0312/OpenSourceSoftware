41eae2fe35f54ddc4eb01d71fb91b9a70467e176
thanh0312 <thanh_0312199@yahoo.com>
2016-08-31 07:50:16 -0400

    Add files via upload

---
+++ PhanSo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace August_24_2016
+{
+    class PhanSo
+    {
+        private int ts, ms;
+        public PhanSo(int tt = 0, int tm = 1)
+        {
+            ts = tt;
+            ms = (tm == 0) ? 1 : tm;
+        }
+        public int ddTS
+        {
+            get { return ts; }
+            set { ts = value; }
+        }
+        public void HienThi()
+        {
+            Console.WriteLine("{0}/{1}", ts, ms);
+        }
+        public override string ToString()
+        {
+            string s = ts.ToString() + "/" + ms.ToString();
+            return s;
+        }
+
+        /*public static PhanSo operator +(PhanSo ta, PhanSo tb)
+        {
+
+        }
+        public static PhanSo operator +(PhanSo ta, PhanSo tb)
+        {
+
+        }
+        public static PhanSo operator +(PhanSo ta, PhanSo tb)
+        {
+
+        }
+        public static PhanSo operator +(PhanSo ta, PhanSo tb)
+        {
+
+        }*/
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace August_24_2016
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhanSo p1 = new PhanSo();
+            PhanSo p2 = new PhanSo(3, 4);
+
+            Console.WriteLine("p1 = {0}", p1.ToString());
+            Console.WriteLine("p1 = {0}", p1);
+
+            int i = 2;
+            Console.WriteLine("i = {0}", i);
+
+
+            Console.ReadKey();
+        }
+    }
+}
